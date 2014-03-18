437261264b52d6d86292f40fd324a3eab5eb6e20
Justin <jsmethie@terraframe.com>
2014-03-18 11:26:44 -0600

    -LocalizationFacade check-in

---
+++ geodashboard-client/src/main/java-gen/stub/com/runwaysdk/geodashboard/localization/LocalizationFacadeDTO.java
@@ -0,0 +1,12 @@
+package com.runwaysdk.geodashboard.localization;
+
+public class LocalizationFacadeDTO extends LocalizationFacadeDTOBase
+ implements com.runwaysdk.generation.loader.Reloadable{
+  private static final long serialVersionUID = 1247995056;
+  
+  public LocalizationFacadeDTO(com.runwaysdk.constants.ClientRequestIF clientRequest)
+  {
+    super(clientRequest);
+  }
+  
+}
