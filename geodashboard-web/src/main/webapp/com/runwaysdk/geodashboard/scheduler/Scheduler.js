9830bf7e1a3e2b66c3268097f2b1a1b6dd56d062
Justin <jsmethie@terraframe.com>
2014-04-09 11:40:38 -0600

    Partial scheduler UI refactor

--- geodashboard-web/src/main/webapp/com/runwaysdk/geodashboard/scheduler/Scheduler.js
+++ geodashboard-web/src/main/webapp/com/runwaysdk/geodashboard/scheduler/Scheduler.js
@@ -223,9 +223,7 @@
       },
       
       _openEditMenu : function(row, job) {
-        var jobMetadata = this._table.getDataSource().getMetadataQueryDTO();
-        
-        var dialog = this.getFactory().newDialog(this.localize("editJobTitle"), {width: "500px"});
+        var dialog = this.getFactory().newDialog(this.localize("editJobTitle"), {minWidth:730});
         
         row.addClassName("row_selected");
         dialog.addDestroyEventListener(function() {
@@ -240,7 +238,7 @@
           descriptionInput.setValue(job.getDescription().getLocalizedValue());
           form.addFormEntry(job.getDescriptionMd(), descriptionInput);          
         }
-        else if(this._user.isFirstNameReadable())
+        else if(job.isDescriptionReadable())
         {
           var label = job.getDescriptionMd().getDisplayLabel();        
           var entry = new com.runwaysdk.geodashboard.ReadEntry('description', label, job ? job.getLocalizedValue() : "");
@@ -249,10 +247,10 @@
         
         if(job.isCronExpressionWritable())
         {
-          var cronInput = new com.runwaysdk.geodashboard.CronInput("cron");
-          cronInput.setValue(job.getCronExpression());
+//          var cronInput = new com.runwaysdk.geodashboard.CronInput("cron");
+//          cronInput.setValue(job.getCronExpression());
           
-          form.addEntry(new com.runwaysdk.geodashboard.FormEntry(this.localize("scheduledRun"), cronInput));
+          form.addEntry(new com.runwaysdk.geodashboard.CronEntry("cron"));
         }
         
         dialog.appendContent(form);
@@ -295,6 +293,10 @@
         dialog.addButton(that.localize("submit"), handleSubmit, null, {class:'btn btn-primary'});
         dialog.addButton(that.localize("cancel"), handleCancel, null, {class:'btn'});                        
         dialog.render();
+        
+        if (jcf != null && jcf.customForms != null) {
+          jcf.customForms.replaceAll();
+        }
             
         return false;
       },
