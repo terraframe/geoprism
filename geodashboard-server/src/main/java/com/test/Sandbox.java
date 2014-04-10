e687f06f26a3f2703cb59ab97efded4f70f11c48
jmapping <jlewis@terraframe.com>
2014-04-10 15:04:14 -0600

    fixing merge issues

--- geodashboard-server/src/main/java/com/test/Sandbox.java
+++ geodashboard-server/src/main/java/com/test/Sandbox.java
@@ -1,6 +1,5 @@
 package com.test;
 
-<<<<<<< Updated upstream
 import com.runwaysdk.geodashboard.gis.model.FeatureType;
 import com.runwaysdk.geodashboard.gis.model.Layer;
 import com.runwaysdk.geodashboard.gis.model.Map;
@@ -18,109 +17,6 @@ import com.runwaysdk.geodashboard.gis.model.impl.ThematicStyleImpl;
 import com.runwaysdk.geodashboard.gis.sld.SLDMapVisitor;
 import com.runwaysdk.geodashboard.gis.sld.WellKnownName;
 
-public class Sandbox
-{
-  public static void main(String[] args)
-  {
-    
-    Map map = new MapImpl("My Map");
-    
-    Layer layer1 = new LayerImpl("Layer 1");
-    layer1.setVirtual(true);
-    layer1.setFeatureType(FeatureType.POINT);
-    map.addLayer(layer1);
-    
-    Style style1 = new StyleImpl();
-    style1.setName("Style 1.1");
-    
-    // point
-    style1.setPointSize(3);
-    style1.setPointStroke("#000000");
-    style1.setPointFill("#fffeee");
-    style1.setPointStrokeWidth(1);
-    style1.setPointOpacity(0.4);
-    style1.setPointRotation(6);
-    style1.setPointStrokeWidth(8);
-    style1.setPointWellKnownName(WellKnownName.STANDARD.CIRCLE.getSymbol());
-    // polygon
-    style1.setPolygonFill("#eeeeee");
-    style1.setPolygonStroke("#000000");
-    style1.setPolygonStrokeWidth(4);
-    
-    layer1.addStyle(style1);    
-    
-
-    ThematicStyleImpl style2 = new ThematicStyleImpl();
-    style2.setAttribute("testAttribute");
-    // point
-    style2.setName("Style 1.2");
-    style2.setPointFill("#999eee");
-    style2.setPointSize(1);
-    style2.setPointStroke("#008800");
-    style2.setPointStrokeWidth(2);
-    style2.setPointOpacity(1.0);
-    style2.setPointRotation(3);
-    style2.setPointStrokeWidth(2);
-    // polygon
-    style2.setPolygonFill("#efefef");
-    style2.setPolygonStroke("#ff0000");
-    style2.setPolygonStrokeWidth(3);
-    style2.setPointWellKnownName(WellKnownName.STANDARD.SQUARE.getSymbol());
-
-    layer1.addStyle(style2);    
-    
-    
-    Equal a = new EqualImpl();
-    a.setValue("1");
-
-    Equal b = new EqualImpl();
-    b.setValue("2");
-
-    Or or1 = new OrImpl();
-    or1.setThematicStyle(style2);
-    or1.setLeftCondition(a);
-    or1.setRightCondition(b);
-
-    Equal c = new EqualImpl();
-    c.setValue("8");
-    
-    Equal d = new EqualImpl();
-    d.setValue("9");
-    
-    Or or2 = new OrImpl();
-    or2.setThematicStyle(style2);
-    or2.setLeftCondition(c);
-    or2.setRightCondition(d);
-    
-    And and = new AndImpl();
-    and.setThematicStyle(style2);
-    and.setLeftCondition(or1);
-    and.setRightCondition(or2);
-    
-    style2.setCondition(and);
-    
-    SLDMapVisitor visitor = new SLDMapVisitor();
-    map.accepts(visitor);
-    
-    System.out.println(visitor.getSLD());
-//    
-//    Map map2 = new MapBuilder("My Map")
-//    
-//      .layer("Layer 1").composite(true).featureType(FeatureType.POINT)
-//        .style("Default Style").pointSize(3).pointStrokeWidth(5).pointRotation(5)
-//        .add()
-//      
-//      .layer("Layer 2").featureType(FeatureType.POLYGON)
-//        .tStyle("Thematic Style").attribute("foo")
-//        .add()
-//      
-//    .build();
-//
-//    SLDMapVisitor visitor2 = new SLDMapVisitor();
-//    map2.accepts(visitor2);
-//    System.out.println(visitor2.getSLD());
-  }
-=======
 import it.geosolutions.geoserver.rest.GeoServerRESTPublisher;
 import it.geosolutions.geoserver.rest.GeoServerRESTReader;
 import it.geosolutions.geoserver.rest.encoder.GSLayerEncoder;
@@ -168,7 +64,7 @@ public class Sandbox
   private static GeoServerRESTPublisher publisher;
 
   private static GeoServerRESTReader    reader;
-  
+
   public static final int               SRS_CODE    = 4326;
 
   public static final String            SRS         = "EPSG:" + SRS_CODE;
@@ -182,8 +78,8 @@ public class Sandbox
   public static int                     MAXX_INDEX  = 2;
 
   public static int                     MAXY_INDEX  = 3;
-  
-  private static Log         log              = LogFactory.getLog(Sandbox.class);
+
+  private static Log                    log         = LogFactory.getLog(Sandbox.class);
 
   static class GeoserverProps
   {
@@ -324,20 +220,19 @@ public class Sandbox
     }
   }
 
-   /**
-   * Checks if the cache directory exists. This method does not check what
-   tiles
+  /**
+   * Checks if the cache directory exists. This method does not check what tiles
    * or zoom levels have been cached.
-   *
+   * 
    * @param cacheName
    * @return
    */
-   public static boolean cacheExists(String cacheName)
-   {
-   String cacheDir = GeoserverProperties.getGeoserverGWCDir() + cacheName;
-   File cache = new File(cacheDir);
-   return cache.exists();
-   }
+  public static boolean cacheExists(String cacheName)
+  {
+    String cacheDir = GeoserverProperties.getGeoserverGWCDir() + cacheName;
+    File cache = new File(cacheDir);
+    return cache.exists();
+  }
 
   /**
    * Gets all styles declared in Geoserver for the workspace.
@@ -419,7 +314,7 @@ public class Sandbox
 
     }
   }
-  
+
   /**
    * Calculates the bounding box of a specific layer.
    * 
@@ -433,7 +328,7 @@ public class Sandbox
 
     return getBBOX(view);
   }
-  
+
   /**
    * Calculates the bounding box of all the layers.
    * 
@@ -515,7 +410,7 @@ public class Sandbox
     // create the layer if it does not exist
     if (layerExists(layer))
     {
-//      log.info("The layer [" + layer + "] already exists in geoserver.");
+      // log.info("The layer [" + layer + "] already exists in geoserver.");
       System.out.println("The layer [" + layer + "] already exists in geoserver.");
       return;
     }
@@ -541,22 +436,23 @@ public class Sandbox
       le.setDefaultStyle(styleName);
       le.setEnabled(true);
 
-      if (getPublisher().publishDBLayer(GeoserverProperties.getWorkspace(), GeoserverProperties.getStore(), fte, le))
+      if (getPublisher().publishDBLayer(GeoserverProperties.getWorkspace(),
+          GeoserverProperties.getStore(), fte, le))
       {
-//        log.info("Created the layer [" + layer + "] in geoserver.");
+        // log.info("Created the layer [" + layer + "] in geoserver.");
         System.out.println("Failed to create the layer [" + layer + "] in geoserver.");
         return;
       }
       else
       {
-//        log.warn("Failed to create the layer [" + layer + "] in geoserver.");
+        // log.warn("Failed to create the layer [" + layer + "] in geoserver.");
         System.out.println("Failed to create the layer [" + layer + "] in geoserver.");
         return;
       }
-      
+
     }
   }
-  
+
   /**
    * Removes the layer from geoserver.
    * 
@@ -575,50 +471,139 @@ public class Sandbox
       log.warn("Failed to remove the layer for [" + layer + "].");
     }
   }
-  
 
   public static void main(String[] args) throws MalformedURLException
   {
 
-    GeoserverProps props = new GeoserverProps();
-    
-    
+    Map map = new MapImpl("My Map");
 
-     GeoServerRESTReader reader = new
-     GeoServerRESTReader(props.getLocalPath(), props.getAdminUser(),
-     props.getAdminPassword());
-     
-//     System.out.println(reader.getWorkspaceNames());
-//     
-//     System.out.println(reader.getResource(reader.getLayer("poi")));
+    Layer layer1 = new LayerImpl("Layer 1");
+    layer1.setVirtual(true);
+    layer1.setFeatureType(FeatureType.POINT);
+    map.addLayer(layer1);
 
-     GeoServerRESTPublisher publisher = new
-     GeoServerRESTPublisher(props.getLocalPath(), props.getAdminUser(),
-     props.getAdminPassword());
+    Style style1 = new StyleImpl();
+    style1.setName("Style 1.1");
 
+    // point
+    style1.setPointSize(3);
+    style1.setPointStroke("#000000");
+    style1.setPointFill("#fffeee");
+    style1.setPointStrokeWidth(1);
+    style1.setPointOpacity(0.4);
+    style1.setPointRotation(6);
+    style1.setPointStrokeWidth(8);
+    style1.setPointWellKnownName(WellKnownName.STANDARD.CIRCLE.getSymbol());
+    // polygon
+    style1.setPolygonFill("#eeeeee");
+    style1.setPolygonStroke("#000000");
+    style1.setPolygonStrokeWidth(4);
+
+    layer1.addStyle(style1);
+
+    ThematicStyleImpl style2 = new ThematicStyleImpl();
+    style2.setAttribute("testAttribute");
+    // point
+    style2.setName("Style 1.2");
+    style2.setPointFill("#999eee");
+    style2.setPointSize(1);
+    style2.setPointStroke("#008800");
+    style2.setPointStrokeWidth(2);
+    style2.setPointOpacity(1.0);
+    style2.setPointRotation(3);
+    style2.setPointStrokeWidth(2);
+    // polygon
+    style2.setPolygonFill("#efefef");
+    style2.setPolygonStroke("#ff0000");
+    style2.setPolygonStrokeWidth(3);
+    style2.setPointWellKnownName(WellKnownName.STANDARD.SQUARE.getSymbol());
+
+    layer1.addStyle(style2);
+
+    Equal a = new EqualImpl();
+    a.setValue("1");
+
+    Equal b = new EqualImpl();
+    b.setValue("2");
+
+    Or or1 = new OrImpl();
+    or1.setThematicStyle(style2);
+    or1.setLeftCondition(a);
+    or1.setRightCondition(b);
+
+    Equal c = new EqualImpl();
+    c.setValue("8");
+
+    Equal d = new EqualImpl();
+    d.setValue("9");
+
+    Or or2 = new OrImpl();
+    or2.setThematicStyle(style2);
+    or2.setLeftCondition(c);
+    or2.setRightCondition(d);
+
+    And and = new AndImpl();
+    and.setThematicStyle(style2);
+    and.setLeftCondition(or1);
+    and.setRightCondition(or2);
+
+    style2.setCondition(and);
+
+    SLDMapVisitor visitor = new SLDMapVisitor();
+    map.accepts(visitor);
+
+    System.out.println(visitor.getSLD());
     //
-    // System.out.println(getReader().getSLD("burg"));
+    // Map map2 = new MapBuilder("My Map")
+    //
+    // .layer("Layer 1").composite(true).featureType(FeatureType.POINT)
+    // .style("Default Style").pointSize(3).pointStrokeWidth(5).pointRotation(5)
+    // .add()
+    //
+    // .layer("Layer 2").featureType(FeatureType.POLYGON)
+    // .tStyle("Thematic Style").attribute("foo")
+    // .add()
+    //
+    // .build();
+    //
+    // SLDMapVisitor visitor2 = new SLDMapVisitor();
+    // map2.accepts(visitor2);
+    // System.out.println(visitor2.getSLD());
 
+    // ///////////////
 
-//    System.out.println(Sandbox.getLayers());
-    
-//    System.out.println("Layer exists = " + Sandbox.layerExists("poi"));
-    
-    System.out.println("Style exists = " + Sandbox.styleExists("poi"));
-    
-    if(Sandbox.layerExists("poi")){
-      System.out.println("Layer exists = " + Sandbox.layerExists("poi_test"));
-      System.out.println("Now lets remove it");
-      
-//      Sandbox.removeLayer("poi_test");
-    }
-    else{
-      Sandbox.publishLayer("poi", "poi");
-    }
+    // GeoserverProps props = new GeoserverProps();
+    //
+    // GeoServerRESTReader reader = new
+    // GeoServerRESTReader(props.getLocalPath(), props.getAdminUser(),
+    // props.getAdminPassword());
+    //
+    // // System.out.println(reader.getWorkspaceNames());
+    // //
+    // // System.out.println(reader.getResource(reader.getLayer("poi")));
+    //
+    // GeoServerRESTPublisher publisher = new
+    // GeoServerRESTPublisher(props.getLocalPath(),
+    // props.getAdminUser(), props.getAdminPassword());
+    //
+    // //
+    // // System.out.println(getReader().getSLD("burg"));
+    //
+    // // System.out.println(Sandbox.getLayers());
+    //
+    // // System.out.println("Layer exists = " + Sandbox.layerExists("poi"));
+    //
+    // System.out.println("Style exists = " + Sandbox.styleExists("poi"));
+    //
+    // if (Sandbox.layerExists("poi"))
+    // {
+    // System.out.println("Layer exists = " + Sandbox.layerExists("poi_test"));
+    // System.out.println("Now lets remove it");
+    //
+    // // Sandbox.removeLayer("poi_test");
+    // }
 
-//    Sandbox.refresh();
+    // Sandbox.refresh();
 
   }
-
->>>>>>> Stashed changes
 }
