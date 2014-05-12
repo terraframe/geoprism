c1ca6df787dea8bc17ed5740973b421a61b3b363
jmapping <jlewis@terraframe.com>
2014-05-12 13:18:09 -0600

    updated controller to persist maps for sessions

--- geodashboard-client/src/main/java-gen/stub/com/runwaysdk/geodashboard/gis/persist/DashboardMapController.java
+++ geodashboard-client/src/main/java-gen/stub/com/runwaysdk/geodashboard/gis/persist/DashboardMapController.java
@@ -133,16 +133,8 @@ public class DashboardMapController extends DashboardMapControllerBase implement
   @Override
   public void createMapForSession() throws IOException, ServletException
   {
-
-    ClientRequestIF clientRequest = this.getClientRequest();
-    
+    ClientRequestIF clientRequest = this.getClientRequest(); 
     DashboardMapDTO map = com.runwaysdk.geodashboard.SessionEntryDTO.createMapForSession(clientRequest);
-    
-    // THIS NEEDS TO BE REPLACED WITH NEW SESSION ORIENTED IMPLEMENTATION
-//    DashboardMapDTO map = new DashboardMapDTO(clientRequest);
-//    map.setName("test_map");
-//    map.apply();  //// commented out to prevent frustration with page reloads until cleanup code is in place
- 
     req.setAttribute("mapId", map.getId());
     render("dashboardViewer.jsp");
   }
