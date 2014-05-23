888f35f22f50011ee987eea49699508595311105
Justin <jsmethie@terraframe.com>
2014-05-23 15:10:16 -0600

    Update of test universal hierarchy

--- geodashboard-test/src/test/java/com/runwaysdk/geodashboard/gis/shapefile/Sandbox.java
+++ geodashboard-test/src/test/java/com/runwaysdk/geodashboard/gis/shapefile/Sandbox.java
@@ -16,22 +16,41 @@ public class Sandbox
         Universal.getStrategy().initialize(AllowedIn.CLASS);
 
         Universal country = new Universal();
-        country.getDisplayLabel().setValue("USA");
-        country.setUniversalId("USA");
+        country.getDisplayLabel().setValue("Cambodia");
+        country.setUniversalId("Cambodia");
         country.apply();
 
-        Universal city = new Universal();
-        city.getDisplayLabel().setValue("City");
-        city.setUniversalId("City");
-        city.apply();
+        Universal municipality = new Universal();
+        municipality.getDisplayLabel().setValue("Municipality");
+        municipality.setUniversalId("Municipality");
+        municipality.apply();
 
-        Universal bike = new Universal();
-        bike.getDisplayLabel().setValue("Bike depot");
-        bike.setUniversalId("Bike depot");
-        bike.apply();
+        Universal province = new Universal();
+        province.getDisplayLabel().setValue("Province");
+        province.setUniversalId("Province");
+        province.apply();
 
-        city.addLink(country, AllowedIn.CLASS);
-        bike.addLink(city, AllowedIn.CLASS);
+        Universal district = new Universal();
+        district.getDisplayLabel().setValue("District");
+        district.setUniversalId("District");
+        district.apply();
+
+        Universal commune = new Universal();
+        commune.getDisplayLabel().setValue("Commune");
+        commune.setUniversalId("Commune");
+        commune.apply();
+
+        Universal village = new Universal();
+        village.getDisplayLabel().setValue("Village");
+        village.setUniversalId("village");
+        village.apply();
+
+        province.addLink(country, AllowedIn.CLASS);
+        municipality.addLink(country, AllowedIn.CLASS);
+        district.addLink(province, AllowedIn.CLASS);
+        district.addLink(municipality, AllowedIn.CLASS);
+        commune.addLink(district, AllowedIn.CLASS);
+        village.addLink(commune, AllowedIn.CLASS);
       }
     }.execute();
   }
