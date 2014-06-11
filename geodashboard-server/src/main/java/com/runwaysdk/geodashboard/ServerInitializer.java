853858ca34f1696c288256c1419bc201325c6a42
justin <jnaifeh@terraframe.com>
2014-06-11 17:37:06 -0400

    added logging code to fix error where maps were not being deleted.

--- geodashboard-server/src/main/java/com/runwaysdk/geodashboard/ServerInitializer.java
+++ geodashboard-server/src/main/java/com/runwaysdk/geodashboard/ServerInitializer.java
@@ -1,5 +1,8 @@
 package com.runwaysdk.geodashboard;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.runwaysdk.generation.loader.Reloadable;
 import com.runwaysdk.geodashboard.gis.geoserver.GeoserverInitializer;
 import com.runwaysdk.gis.StrategyInitializer;
@@ -8,20 +11,49 @@ import com.runwaysdk.system.scheduler.SchedulerManager;
 
 public class ServerInitializer implements Reloadable
 {
+
+  private static final Log log = LogFactory.getLog(ServerInitializer.class);
+
   @Request
   public static void initialize()
   {
-    StrategyInitializer.startUp();
-    SchedulerManager.start();
-    SessionEntry.deleteAll();
-    GeoserverInitializer.setup();
+    int tasks = 0;
+    try
+    {
+      StrategyInitializer.startUp();
+
+      tasks++;
+      SchedulerManager.start();
+
+      tasks++;
+      SessionEntry.deleteAll();
+
+      tasks++;
+      GeoserverInitializer.setup();
+    }
+    catch (Throwable t)
+    {
+      log.fatal("Cannot start up. Failed on task [" + tasks + "].", t);
+    }
   }
 
   @Request
   public static void destroy()
   {
-    StrategyInitializer.tearDown();
-    SchedulerManager.shutdown();
-    SessionEntry.deleteAll();
+    int tasks = 0;
+    try
+    {
+      StrategyInitializer.tearDown();
+      
+      tasks++;
+      SchedulerManager.shutdown();
+      
+      tasks++;
+      SessionEntry.deleteAll();
+    }
+    catch (Throwable t)
+    {
+      log.fatal("Cannot start up. Failed on task [" + tasks + "].", t);
+    }
   }
 }
