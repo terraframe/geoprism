0abba44c43af8110ac8dcbaa7646ae4b37492e16
justin <jnaifeh@terraframe.com>
2014-06-19 11:07:31 -0400

    local.

--- geodashboard-test/src/test/java/com/runwaysdk/geodashboard/gis/geoserver/GeoserverTest.java
+++ geodashboard-test/src/test/java/com/runwaysdk/geodashboard/gis/geoserver/GeoserverTest.java
@@ -134,7 +134,7 @@ import com.runwaysdk.util.FileIO;
     
     protected static Dashboard dashboard;
     
-    private static boolean keepData = true;
+    private static boolean keepData = false;
 
     protected static final Log            log            = LogFactory.getLog(GeoserverTest.class);
 
