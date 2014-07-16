71ad8cdde1ae31088f2399a2aa6dbc9e5a0b39c3
jsmethie <jsmethie@terraframe.com>
2014-07-16 15:08:02 +0000

    Merge

--- geodashboard-server/src/main/java/com/runwaysdk/geodashboard/StrategyContextListener.java
+++ geodashboard-server/src/main/java/com/runwaysdk/geodashboard/StrategyContextListener.java
@@ -1,6 +1,8 @@
 package com.runwaysdk.geodashboard;
 
 import com.runwaysdk.generation.loader.Reloadable;
+import com.runwaysdk.geodashboard.ontology.Classifier;
+import com.runwaysdk.geodashboard.ontology.ClassifierIsARelationship;
 import com.runwaysdk.gis.StrategyInitializer;
 
 public class StrategyContextListener implements ServerContextListener, Reloadable
@@ -9,11 +11,13 @@ public class StrategyContextListener implements ServerContextListener, Reloadabl
   public void startup()
   {
     StrategyInitializer.startUp();
+    Classifier.getStrategy().initialize(ClassifierIsARelationship.CLASS);
   }
 
   @Override
   public void shutdown()
   {
     StrategyInitializer.shutDown();
+    Classifier.getStrategy().shutdown();
   }
 }
