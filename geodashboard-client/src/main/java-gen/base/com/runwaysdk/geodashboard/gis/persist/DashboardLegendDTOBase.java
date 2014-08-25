517af9b24f054ffe25054803538733fc79857598
jmapping <jlewis@terraframe.com>
2014-08-25 16:28:16 -0600

    legend

---
+++ geodashboard-client/src/main/java-gen/base/com/runwaysdk/geodashboard/gis/persist/DashboardLegendDTOBase.java
@@ -0,0 +1,199 @@
+package com.runwaysdk.geodashboard.gis.persist;
+
+@com.runwaysdk.business.ClassSignature(hash = -562019781)
+public abstract class DashboardLegendDTOBase extends com.runwaysdk.business.StructDTO implements com.runwaysdk.generation.loader.Reloadable
+{
+  public final static String CLASS = "com.runwaysdk.geodashboard.gis.persist.DashboardLegend";
+  private static final long serialVersionUID = -562019781;
+  
+  protected DashboardLegendDTOBase(com.runwaysdk.constants.ClientRequestIF clientRequest)
+  {
+    super(clientRequest);
+  }
+  
+  /**
+  * Copy Constructor: Duplicates the values and attributes of the given StructDTO into a new DTO.
+  * 
+  * @param structDTO The StructDTO to duplicate
+  * @param clientRequest The clientRequest this DTO should use to communicate with the server.
+  */
+  protected DashboardLegendDTOBase(com.runwaysdk.business.StructDTO structDTO, com.runwaysdk.constants.ClientRequestIF clientRequest)
+  {
+    super(structDTO, clientRequest);
+  }
+  
+  protected java.lang.String getDeclaredType()
+  {
+    return CLASS;
+  }
+  
+  public static java.lang.String ID = "id";
+  public static java.lang.String KEYNAME = "keyName";
+  public static java.lang.String LEGENDXPOSITION = "legendXPosition";
+  public static java.lang.String LEGENDYPOSITION = "legendYPosition";
+  public static java.lang.String SITEMASTER = "siteMaster";
+  public String getKeyName()
+  {
+    return getValue(KEYNAME);
+  }
+  
+  public void setKeyName(String value)
+  {
+    if(value == null)
+    {
+      setValue(KEYNAME, "");
+    }
+    else
+    {
+      setValue(KEYNAME, value);
+    }
+  }
+  
+  public boolean isKeyNameWritable()
+  {
+    return isWritable(KEYNAME);
+  }
+  
+  public boolean isKeyNameReadable()
+  {
+    return isReadable(KEYNAME);
+  }
+  
+  public boolean isKeyNameModified()
+  {
+    return isModified(KEYNAME);
+  }
+  
+  public final com.runwaysdk.transport.metadata.AttributeCharacterMdDTO getKeyNameMd()
+  {
+    return (com.runwaysdk.transport.metadata.AttributeCharacterMdDTO) getAttributeDTO(KEYNAME).getAttributeMdDTO();
+  }
+  
+  public Integer getLegendXPosition()
+  {
+    return com.runwaysdk.constants.MdAttributeIntegerUtil.getTypeSafeValue(getValue(LEGENDXPOSITION));
+  }
+  
+  public void setLegendXPosition(Integer value)
+  {
+    if(value == null)
+    {
+      setValue(LEGENDXPOSITION, "");
+    }
+    else
+    {
+      setValue(LEGENDXPOSITION, java.lang.Integer.toString(value));
+    }
+  }
+  
+  public boolean isLegendXPositionWritable()
+  {
+    return isWritable(LEGENDXPOSITION);
+  }
+  
+  public boolean isLegendXPositionReadable()
+  {
+    return isReadable(LEGENDXPOSITION);
+  }
+  
+  public boolean isLegendXPositionModified()
+  {
+    return isModified(LEGENDXPOSITION);
+  }
+  
+  public final com.runwaysdk.transport.metadata.AttributeNumberMdDTO getLegendXPositionMd()
+  {
+    return (com.runwaysdk.transport.metadata.AttributeNumberMdDTO) getAttributeDTO(LEGENDXPOSITION).getAttributeMdDTO();
+  }
+  
+  public Integer getLegendYPosition()
+  {
+    return com.runwaysdk.constants.MdAttributeIntegerUtil.getTypeSafeValue(getValue(LEGENDYPOSITION));
+  }
+  
+  public void setLegendYPosition(Integer value)
+  {
+    if(value == null)
+    {
+      setValue(LEGENDYPOSITION, "");
+    }
+    else
+    {
+      setValue(LEGENDYPOSITION, java.lang.Integer.toString(value));
+    }
+  }
+  
+  public boolean isLegendYPositionWritable()
+  {
+    return isWritable(LEGENDYPOSITION);
+  }
+  
+  public boolean isLegendYPositionReadable()
+  {
+    return isReadable(LEGENDYPOSITION);
+  }
+  
+  public boolean isLegendYPositionModified()
+  {
+    return isModified(LEGENDYPOSITION);
+  }
+  
+  public final com.runwaysdk.transport.metadata.AttributeNumberMdDTO getLegendYPositionMd()
+  {
+    return (com.runwaysdk.transport.metadata.AttributeNumberMdDTO) getAttributeDTO(LEGENDYPOSITION).getAttributeMdDTO();
+  }
+  
+  public String getSiteMaster()
+  {
+    return getValue(SITEMASTER);
+  }
+  
+  public boolean isSiteMasterWritable()
+  {
+    return isWritable(SITEMASTER);
+  }
+  
+  public boolean isSiteMasterReadable()
+  {
+    return isReadable(SITEMASTER);
+  }
+  
+  public boolean isSiteMasterModified()
+  {
+    return isModified(SITEMASTER);
+  }
+  
+  public final com.runwaysdk.transport.metadata.AttributeCharacterMdDTO getSiteMasterMd()
+  {
+    return (com.runwaysdk.transport.metadata.AttributeCharacterMdDTO) getAttributeDTO(SITEMASTER).getAttributeMdDTO();
+  }
+  
+  public static DashboardLegendDTO get(com.runwaysdk.constants.ClientRequestIF clientRequest, String id)
+  {
+    com.runwaysdk.business.EntityDTO dto = (com.runwaysdk.business.EntityDTO)clientRequest.get(id);
+    
+    return (DashboardLegendDTO) dto;
+  }
+  
+  public void apply()
+  {
+    if(isNewInstance())
+    {
+      getRequest().createStruct(this);
+    }
+    else
+    {
+      getRequest().update(this);
+    }
+  }
+  public void delete()
+  {
+    getRequest().delete(this.getId());
+  }
+  
+  public static com.runwaysdk.geodashboard.gis.persist.DashboardLegendQueryDTO getAllInstances(com.runwaysdk.constants.ClientRequestIF clientRequest, String sortAttribute, Boolean ascending, Integer pageSize, Integer pageNumber)
+  {
+    return (com.runwaysdk.geodashboard.gis.persist.DashboardLegendQueryDTO) clientRequest.getAllInstances(com.runwaysdk.geodashboard.gis.persist.DashboardLegendDTO.CLASS, sortAttribute, ascending, pageSize, pageNumber);
+  }
+  
+}
