8dc50fe69c8f1dcbbb709fca6d2c59004cd57139
Justin Smethie <jsmethie@terraframe.com>
2015-04-09 13:10:31 -0600

    -Ticket #401

--- geodashboard-server/src/main/java/com/runwaysdk/geodashboard/gis/geoserver/LayerFacade.java
+++ geodashboard-server/src/main/java/com/runwaysdk/geodashboard/gis/geoserver/LayerFacade.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 import com.runwaysdk.dataaccess.database.Database;
 import com.runwaysdk.dataaccess.transaction.Transaction;
+import com.runwaysdk.generation.loader.Reloadable;
 import com.runwaysdk.geodashboard.SessionParameterFacade;
 import com.runwaysdk.geodashboard.gis.geoserver.GeoserverInitializer.SessionPredicate;
 import com.runwaysdk.geodashboard.gis.persist.DashboardLayer;
 import com.runwaysdk.geodashboard.util.Iterables;
 import com.runwaysdk.session.Request;
 
-public class LayerFacade
+public class LayerFacade implements Reloadable
 {
   @Request
   public static void cleanupUnusedLayers()
