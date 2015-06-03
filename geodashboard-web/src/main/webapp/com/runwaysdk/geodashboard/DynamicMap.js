67a7847dcbc1b04e2640a05ab19d304cf79291ec
justinlewis <j.mapping@gmail.com>
2015-06-03 15:14:28 -0600

    fixed non scrolling select drop downs

--- geodashboard-web/src/main/webapp/com/runwaysdk/geodashboard/DynamicMap.js
+++ geodashboard-web/src/main/webapp/com/runwaysdk/geodashboard/DynamicMap.js
@@ -2335,6 +2335,7 @@
       },
       
       _addLayerFormControls : function(){
+    	var that = this;
         
         $("#secondary-select-box").change(Mojo.Util.bind(this, this._handleSecondaryChange));
         
@@ -2388,6 +2389,20 @@
 
           }
         }
+        
+        // Scroll selector dropdown options on page scroll
+        $("#modal01").scroll(function(){       	
+	        var drops = $(".select-options");
+	    	for(var i=0; i<drops.length; i++){
+	    		var drop = $(drops[i]);
+	    		if(!drop.hasClass("options-hidden")){
+	    			var dropSelector = $(".select-active");
+	    			var diff = dropSelector.offset().top + dropSelector.height() + 2; 
+	    			var diffStr = diff.toString() + "px";
+	    			drop.css({ top: diffStr });
+	    		}
+	    	}
+        });
         
         this._injectFontStylesForDropdown();
       },
@@ -3568,7 +3583,6 @@
         
         $('#control-form-collapse-button').on('click', Mojo.Util.bind(this, this._onClickToggleLeftPanel));
         $('#data-panel-expand-toggle').on('click', Mojo.Util.bind(this, this._onClickToggleRightPanel));
-        
         
         $('.report-height-toggle').on('click', function(e){
           var target = e.target;
