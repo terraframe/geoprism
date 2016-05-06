483a5fe6442d5f094ab40321deb017246d72dd05
rrowlands <rrowlands@terraframe.com>
2016-05-06 11:24:24 -0600

    Progress on PropertiesMerger.

--- geoprism-common/src/main/java/net/geoprism/PropertiesMerger.java
+++ geoprism-common/src/main/java/net/geoprism/PropertiesMerger.java
@@ -27,38 +27,89 @@ public class PropertiesMerger
 {
   private CompositeConfiguration cconfig;
   
-  private File base;
-  
-  private File override;
-  
-  private File export;
-  
   public static void main(String[] args) throws ConfigurationException, ParseException, IOException
   {
     CommandLineParser parser = new DefaultParser();
     Options options = new Options();
-    options.addOption(Option.builder("b").hasArg().argName("base").required().desc("The path to the base properties file.").build());
-    options.addOption(Option.builder("o").hasArg().argName("override").required().desc("The path to the override properties file.").build());
-    options.addOption(Option.builder("e").hasArg().argName("export").required().desc("The path to the export properties file.").build());
+    options.addOption(Option.builder("b").hasArg().argName("baseFile").desc("The path to the base properties file.").build());
+    options.addOption(Option.builder("o").hasArg().argName("overrideFile").desc("The path to the override properties file.").build());
+    options.addOption(Option.builder("e").hasArg().argName("exportFile").desc("The path to the export properties file. A null value defaults to base.").build());
+    options.addOption(Option.builder("B").hasArg().argName("baseDir").desc("The path to the base directory.").build());
+    options.addOption(Option.builder("O").hasArg().argName("overrideDir").desc("The path to the override directory.").build());
+    options.addOption(Option.builder("E").hasArg().argName("exportDir").desc("The path to the export directory. A null value defaults to base.").build());
     CommandLine line = parser.parse( options, args );
-
-    File base = new File(line.getOptionValue("b"));
-    File override = new File(line.getOptionValue("o"));
-    File export = new File(line.getOptionValue("e"));
     
-    PropertiesMerger merger = new PropertiesMerger(base, override, export);
-    merger.export();
+    String sBase = line.getOptionValue("b");
+    String sOverride = line.getOptionValue("o");
+    String sExport = line.getOptionValue("e");
+    String sBaseDir = line.getOptionValue("B");
+    String sOverrideDir = line.getOptionValue("O");
+    String sExportDir = line.getOptionValue("E");
+    
+    if (sBase != null && sOverride != null)
+    {
+      if (sExport == null)
+      {
+        sExport = sBase;
+      }
+      
+      PropertiesMerger merger = new PropertiesMerger();
+      merger.mergeProperties(new File(sBase), new File(sOverride), new File(sExport));
+    }
+    else if (sBaseDir != null && sOverrideDir != null)
+    {
+      if (sExportDir == null)
+      {
+        sExportDir = sBaseDir;
+      }
+      
+      PropertiesMerger merger = new PropertiesMerger();
+      merger.mergeDirectories(new File(sBaseDir), new File(sOverrideDir), new File(sExportDir));
+    }
+    else
+    {
+      throw new RuntimeException("Invalid arguments");
+    }
   }
   
-  public PropertiesMerger(File base, File override, File export)
+  public PropertiesMerger()
   {
-    this.base = base;
-    this.override = override;
-    this.export = export;
+    
   }
   
-  public void export() throws ConfigurationException, IOException
+  public void mergeDirectories(File base, File override, File export) throws ConfigurationException, IOException
   {
+    if (!export.exists())
+    {
+      export.mkdirs();
+    }
+    
+    for (File child : override.listFiles())
+    {
+      String name = child.getName();
+      
+      if (child.isDirectory())
+      {
+        mergeDirectories(new File(base, name), child, new File(export, name));
+      }
+      else if (name.endsWith(".properties"))
+      {
+        mergeProperties(new File(base, name), child, new File(export, name));
+      }
+    }
+  }
+  
+  public void mergeProperties(File base, File override, File export) throws ConfigurationException, IOException
+  {
+    if (!export.exists())
+    {
+      export.createNewFile();
+    }
+    if (!base.exists())
+    {
+      base.createNewFile();
+    }
+    
     Parameters params = new Parameters();
     FileBasedConfigurationBuilder<FileBasedConfiguration> baseBuilder =
         new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
@@ -73,11 +124,6 @@ public class PropertiesMerger
     this.cconfig = new CompositeConfiguration();
     this.cconfig.addConfiguration(overrideBuilder.getConfiguration());
     this.cconfig.addConfiguration(baseBuilder.getConfiguration());
-    
-    if (!export.exists())
-    {
-      export.createNewFile();
-    }
     
     FileBasedConfigurationBuilder<FileBasedConfiguration> exportBuilder =
         new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
