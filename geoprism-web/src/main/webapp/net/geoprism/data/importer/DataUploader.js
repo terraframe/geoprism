be29adda3d4d86c54072c02e8c7c38d7928e472b
Justin Smethie <jsmethie@terraframe.com>
2016-09-16 09:37:35 -0600

    * Refactor to reused runwayService.generateId

--- geoprism-web/src/main/webapp/net/geoprism/data/importer/DataUploader.js
+++ geoprism-web/src/main/webapp/net/geoprism/data/importer/DataUploader.js
@@ -258,7 +258,7 @@
     }   
   }  
   
-  function GeoValidationProblemController($scope, datasetService) {
+  function GeoValidationProblemController($scope, datasetService, runwayService) {
     var controller = this;
     $scope.problem.synonym = null;
     
@@ -373,17 +373,10 @@
       }
     }
     
-    controller.guid = function() {
-      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-          var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-          return v.toString(16);
-      });
-    }
-    
     controller.ignoreDataAtLocation = function() {
       var locationLabel = $scope.problem.label;
       var universal = $scope.problem.universalId;
-      var id = controller.guid();
+      var id = runwayService.generateId();
       
       $scope.problem.resolved = true;
       
@@ -403,8 +396,7 @@
       else{
         config.locationExclusions = [exclusion];
       }
-    }
-    
+    }    
 
     controller.undoAction = function() {
       var locationLabel = $scope.problem.label;
