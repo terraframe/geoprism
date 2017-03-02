a11f30bded81f6db54904ae92445cbf22ca04142
Justin Smethie <jsmethie@terraframe.com>
2017-03-02 15:27:34 -0700

    * ng2 webpack - enable prod mode

--- geoprism-web/src/main/ng2/src/main.ts
+++ geoprism-web/src/main/ng2/src/main.ts
@@ -18,8 +18,12 @@
 ///
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
+
+if (process.env.ENV === 'production') {
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .then(success => console.log('Bootstrap success'))
