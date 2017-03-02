a3abdfc92c996228b0091c8cd06f1c8e9cff146a
Justin Smethie <jsmethie@terraframe.com>
2017-03-02 15:39:30 -0700

    * ng2 development mode

--- geoprism-web/src/main/ng2/config/webpack.dev.js
+++ geoprism-web/src/main/ng2/config/webpack.dev.js
@@ -3,6 +3,8 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
 
+const ENV = process.env.NODE_ENV = process.env.ENV = 'development';
+
 module.exports = webpackMerge(commonConfig, {
   devtool: 'cheap-module-eval-source-map',
 
@@ -14,7 +16,12 @@ module.exports = webpackMerge(commonConfig, {
   },
 
   plugins: [
-    new ExtractTextPlugin('[name].css')
+    new ExtractTextPlugin('[name].css'),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'ENV': JSON.stringify(ENV)
+      }
+    }),    
   ],
 
   devServer: {
