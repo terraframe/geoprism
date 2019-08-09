f472b86c3eed260be10ea18c1854d0b6e62f5a5e
rrowlands <rrowlands@terraframe.com>
2019-08-09 12:53:07 -0600

    Added roles to accounts page.

---
+++ geoprism-client/src/main/java-gen/stub/net/geoprism/account/GeoprismUserViewDTO.java
@@ -0,0 +1,12 @@
+package net.geoprism.account;
+
+public class GeoprismUserViewDTO extends GeoprismUserViewDTOBase
+{
+  private static final long serialVersionUID = 316142724;
+  
+  public GeoprismUserViewDTO(com.runwaysdk.constants.ClientRequestIF clientRequest)
+  {
+    super(clientRequest);
+  }
+  
+}
