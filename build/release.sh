088fcf788d17f4e5a855f8e4a154e8754887129a
rrowlands <rrowlands@terraframe.com>
2020-11-11 16:32:09 -0700

    License headers.

--- build/release.sh
+++ build/release.sh
@@ -1,3 +1,22 @@
+#
+# Copyright (c) 2015 TerraFrame, Inc. All rights reserved.
+#
+# This file is part of Geoprism(tm).
+#
+# Geoprism(tm) is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Geoprism(tm) is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with Geoprism(tm).  If not, see <http://www.gnu.org/licenses/>.
+#
+
 #if curl -f -s --head "http://nexus.terraframe.com/service/local/artifact/maven/redirect?r=allrepos&g=net.geoprism&a=geoprism-web&p=war&v=$GP_VERSION" | head -n 1 | grep "HTTP/1.[01] [23].." > /dev/null; then
 #   echo "The release version $GP_VERSION has already been deployed! Please ensure you are releasing the correct version of geoprism."
 #   exit 1
