9158e03e649866b83cd74cfd63cbbf609e3b28e3
Richard Rowlands <rrowlands@terraframe.com>
2023-10-17 13:16:40 -0600

    chore(graph-repo): transition event test and xml importer test

--- geoprism-graphrepo/src/main/java/net/geoprism/registry/service/GraphRepoServiceIF.java
+++ geoprism-graphrepo/src/main/java/net/geoprism/registry/service/GraphRepoServiceIF.java
@@ -2,10 +2,14 @@ package net.geoprism.registry.service;
 
 import org.springframework.stereotype.Component;
 
+import net.geoprism.registry.model.ServerElement;
+
 @Component
 public interface GraphRepoServiceIF
 {
   public void initialize();
   
   public void refreshMetadataCache();
+  
+  public void deleteObject(ServerElement obj);
 }
