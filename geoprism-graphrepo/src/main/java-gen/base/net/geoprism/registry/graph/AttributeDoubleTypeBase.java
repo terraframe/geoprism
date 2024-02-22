8507664154ab34cb368c830c435f4524da3b794a
jsmethie <jsmethie@terraframe.com>
2024-02-22 10:31:39 -0700

    chore: support for custom attribute double value attribute nodes

--- geoprism-graphrepo/src/main/java-gen/base/net/geoprism/registry/graph/AttributeDoubleTypeBase.java
+++ geoprism-graphrepo/src/main/java-gen/base/net/geoprism/registry/graph/AttributeDoubleTypeBase.java
@@ -1,6 +1,6 @@
 package net.geoprism.registry.graph;
 
-@com.runwaysdk.business.ClassSignature(hash = 1524384537)
+@com.runwaysdk.business.ClassSignature(hash = -1731486382)
 /**
  * This class is generated automatically.
  * DO NOT MAKE CHANGES TO IT - THEY WILL BE OVERWRITTEN
@@ -11,14 +11,80 @@ package net.geoprism.registry.graph;
 public abstract class AttributeDoubleTypeBase extends net.geoprism.registry.graph.AttributeType
 {
   public final static String CLASS = "net.geoprism.registry.graph.AttributeDoubleType";
-  public final static String PRECISION = "precision";
-  public final static String SCALE = "scale";
+  public final static java.lang.String PRECISION = "precision";
+  public final static java.lang.String SCALE = "scale";
+  public final static java.lang.String VALUEVERTEX = "valueVertex";
   @SuppressWarnings("unused")
-  private static final long serialVersionUID = 1524384537;
+  private static final long serialVersionUID = -1731486382;
   
   public AttributeDoubleTypeBase()
   {
     super();
+  }
+  
+  public Integer getPrecision()
+  {
+    return (Integer) this.getObjectValue(PRECISION);
+  }
+  
+  public static com.runwaysdk.dataaccess.MdAttributeIntegerDAOIF getPrecisionMd()
+  {
+    com.runwaysdk.dataaccess.MdClassDAOIF mdClassIF = com.runwaysdk.dataaccess.metadata.MdClassDAO.getMdClassDAO(net.geoprism.registry.graph.AttributeDoubleType.CLASS);
+    return (com.runwaysdk.dataaccess.MdAttributeIntegerDAOIF)mdClassIF.definesAttribute(PRECISION);
+  }
+  
+  public void setPrecision(Integer value)
+  {
+    this.setValue(PRECISION, value);
+  }
+  
+  public Integer getScale()
+  {
+    return (Integer) this.getObjectValue(SCALE);
+  }
+  
+  public static com.runwaysdk.dataaccess.MdAttributeIntegerDAOIF getScaleMd()
+  {
+    com.runwaysdk.dataaccess.MdClassDAOIF mdClassIF = com.runwaysdk.dataaccess.metadata.MdClassDAO.getMdClassDAO(net.geoprism.registry.graph.AttributeDoubleType.CLASS);
+    return (com.runwaysdk.dataaccess.MdAttributeIntegerDAOIF)mdClassIF.definesAttribute(SCALE);
+  }
+  
+  public void setScale(Integer value)
+  {
+    this.setValue(SCALE, value);
+  }
+  
+  public com.runwaysdk.system.metadata.MdVertex getValueVertex()
+  {
+    if (this.getObjectValue(VALUEVERTEX) == null)
+    {
+      return null;
+    }
+    else
+    {
+      return com.runwaysdk.system.metadata.MdVertex.get( (String) this.getObjectValue(VALUEVERTEX));
+    }
+  }
+  
+  public String getValueVertexOid()
+  {
+    return (String) this.getObjectValue(VALUEVERTEX);
+  }
+  
+  public static com.runwaysdk.dataaccess.MdAttributeReferenceDAOIF getValueVertexMd()
+  {
+    com.runwaysdk.dataaccess.MdClassDAOIF mdClassIF = com.runwaysdk.dataaccess.metadata.MdClassDAO.getMdClassDAO(net.geoprism.registry.graph.AttributeDoubleType.CLASS);
+    return (com.runwaysdk.dataaccess.MdAttributeReferenceDAOIF)mdClassIF.definesAttribute(VALUEVERTEX);
+  }
+  
+  public void setValueVertex(com.runwaysdk.system.metadata.MdVertex value)
+  {
+    this.setValue(VALUEVERTEX, value.getOid());
+  }
+  
+  public void setValueVertexId(java.lang.String oid)
+  {
+    this.setValue(VALUEVERTEX, oid);
   }
   
   protected String getDeclaredType()
--- geoprism-graphrepo/src/main/java-gen/stub/net/geoprism/registry/graph/AttributeDoubleType.java
+++ geoprism-graphrepo/src/main/java-gen/stub/net/geoprism/registry/graph/AttributeDoubleType.java
@@ -3,19 +3,111 @@ package net.geoprism.registry.graph;
 import org.commongeoregistry.adapter.metadata.AttributeFloatType;
 import org.commongeoregistry.adapter.metadata.AttributeType;
 
+import com.runwaysdk.constants.MdAttributeBooleanInfo;
 import com.runwaysdk.constants.MdAttributeDoubleInfo;
+import com.runwaysdk.constants.MdAttributeLocalInfo;
+import com.runwaysdk.constants.graph.MdVertexInfo;
+import com.runwaysdk.dataaccess.MdVertexDAOIF;
+import com.runwaysdk.dataaccess.database.Database;
 import com.runwaysdk.dataaccess.metadata.MdAttributeConcreteDAO;
+import com.runwaysdk.dataaccess.metadata.MdAttributeDoubleDAO;
+import com.runwaysdk.dataaccess.metadata.graph.MdVertexDAO;
+import com.runwaysdk.dataaccess.transaction.Transaction;
+import com.runwaysdk.system.metadata.MdVertex;
 
+import net.geoprism.registry.RegistryConstants;
+import net.geoprism.registry.model.ValueNodeStrategy;
 import net.geoprism.registry.model.ValueStrategy;
+import net.geoprism.registry.model.VertexValueStrategy;
 
 public class AttributeDoubleType extends AttributeDoubleTypeBase
 {
+  public static final String PREFIX           = "dv_";
+
+  public static final String VALUE            = "value";
+
   @SuppressWarnings("unused")
-  private static final long serialVersionUID = -352072420;
+  private static final long  serialVersionUID = -352072420;
 
   public AttributeDoubleType()
   {
     super();
+  }
+
+  private String getTableName()
+  {
+    int count = 0;
+
+    String name = PREFIX + count + "DoubleValue";
+
+    if (name.length() > 25)
+    {
+      name = name.substring(0, 25);
+    }
+
+    while (Database.tableExists(name))
+    {
+      count++;
+
+      name = PREFIX + count + "DoubleValue";
+
+      if (name.length() > 25)
+      {
+        name = name.substring(0, 25);
+      }
+    }
+
+    return name;
+  }
+
+  @Override
+  @Transaction
+  public void apply()
+  {
+    // Create the value vertex class
+    if (this.isNew() && !this.isAppliedToDb())
+    {
+      String tableName = this.getTableName();
+      
+      MdVertexDAOIF superVertex = MdVertexDAO.getMdVertexDAO(AttributeBasicValue.CLASS);
+
+      MdVertexDAO mdVertex = MdVertexDAO.newInstance();
+      mdVertex.setValue(MdVertexInfo.PACKAGE, RegistryConstants.UNIVERSAL_GRAPH_PACKAGE + ".value");
+      mdVertex.setValue(MdVertexInfo.NAME, tableName);
+      mdVertex.setValue(MdVertexInfo.ENABLE_CHANGE_OVER_TIME, MdAttributeBooleanInfo.FALSE);
+      mdVertex.setValue(MdVertexInfo.GENERATE_SOURCE, MdAttributeBooleanInfo.FALSE);
+      mdVertex.setValue(MdVertexInfo.DB_CLASS_NAME, tableName);
+      mdVertex.setValue(MdVertexInfo.SUPER_MD_VERTEX, superVertex.getOid());
+      mdVertex.apply();
+
+      // Create the value attribute
+      MdAttributeDoubleDAO mdAttribute = MdAttributeDoubleDAO.newInstance();
+      mdAttribute.setValue(MdAttributeDoubleInfo.DEFINING_MD_CLASS, mdVertex.getOid());
+      mdAttribute.setValue(MdAttributeDoubleInfo.NAME, VALUE);
+      mdAttribute.setStructValue(MdAttributeDoubleInfo.DISPLAY_LABEL, MdAttributeLocalInfo.DEFAULT_LOCALE, "Value");
+      mdAttribute.setValue(MdAttributeDoubleInfo.LENGTH, getPrecision());
+      mdAttribute.setValue(MdAttributeDoubleInfo.DECIMAL, getScale());
+      mdAttribute.setValue(MdAttributeDoubleInfo.REQUIRED, MdAttributeBooleanInfo.TRUE);
+      mdAttribute.apply();
+
+      this.setValueVertexId(mdVertex.getOid());
+    }
+
+    super.apply();
+  }
+
+  @Override
+  @Transaction
+  public void delete()
+  {
+    MdVertex mdVertex = this.getValueVertex();
+
+    super.delete();
+
+    if (mdVertex != null)
+    {
+      mdVertex.delete();
+    }
   }
 
   public Integer getPrecision()
@@ -61,18 +153,14 @@ public class AttributeDoubleType extends AttributeDoubleTypeBase
   @Override
   public ValueStrategy getStrategy()
   {
-    throw new UnsupportedOperationException();
-    // TODO: HEADS UP
-    // if (!this.getIsChangeOverTime())
-    // {
-    // return new VertexValueStrategy(this);
-    // }
-    // else
-    // {
-    // return new ValueNodeStrategy(this,
-    // MdVertexDAO.getMdVertexDAO(AttributeCharacterValue.CLASS),
-    // AttributeCharacterValue.VALUE);
-    // }
+    if (!this.getIsChangeOverTime())
+    {
+      return new VertexValueStrategy(this);
+    }
+    else
+    {
+      return new ValueNodeStrategy(this, MdVertexDAO.get(this.getValueVertexOid()), VALUE);
+    }
   }
 
 }
