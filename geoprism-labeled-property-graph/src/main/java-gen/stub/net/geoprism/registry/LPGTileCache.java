e1f16eae706475544a4136f0ae0ff7f2643d0a8b
terraframe-builder <builder@terraframe.com>
2024-05-13 16:57:29 +0000

    License headers

--- geoprism-labeled-property-graph/src/main/java-gen/stub/net/geoprism/registry/LPGTileCache.java
+++ geoprism-labeled-property-graph/src/main/java-gen/stub/net/geoprism/registry/LPGTileCache.java
@@ -1,18 +1,18 @@
 /**
  * Copyright (c) 2023 TerraFrame, Inc. All rights reserved.
- * <p>
+ *
  * This file is part of Geoprism(tm).
- * <p>
+ *
  * Geoprism(tm) is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * <p>
+ *
  * Geoprism(tm) is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * <p>
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with Geoprism(tm).  If not, see <http://www.gnu.org/licenses/>.
  */
