29211fafcf839cfa29ac8e4eb9c812c6306cfd14
jsmethie <jsmethie@terraframe.com>
2025-05-20 15:59:06 -0600

    chore: support for defining business edge types in a request

---
+++ geoprism-graphrepo/src/main/java/net/geoprism/registry/service/request/BusinesEdgeTypeService.java
@@ -0,0 +1,64 @@
+package net.geoprism.registry.service.request;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.runwaysdk.session.Request;
+import com.runwaysdk.session.RequestType;
+
+import net.geoprism.registry.BusinessEdgeType;
+import net.geoprism.registry.JsonCollectors;
+import net.geoprism.registry.service.business.BusinessEdgeTypeBusinessServiceIF;
+
+@Service
+public class BusinesEdgeTypeService implements BusinessEdgeTypeServiceIF
+{
+  @Autowired
+  private BusinessEdgeTypeBusinessServiceIF service;
+
+  @Override
+  @Request(RequestType.SESSION)
+  public void update(String sessionId, String code, JsonObject object)
+  {
+    BusinessEdgeType type = this.service.getByCode(code);
+
+    this.service.update(type, object);
+  }
+
+  @Override
+  @Request(RequestType.SESSION)
+  public void delete(String sessionId, String code)
+  {
+    BusinessEdgeType type = this.service.getByCode(code);
+
+    this.service.delete(type);
+  }
+
+  @Override
+  @Request(RequestType.SESSION)
+  public JsonArray getAll(String sessionId)
+  {
+    return this.service.getAll().stream().map(type -> this.service.toJSON(type)).collect(JsonCollectors.toJsonArray());
+  }
+
+  @Override
+  @Request(RequestType.SESSION)
+  public JsonObject getByCode(String sessionId, String code)
+  {
+    return this.service.toJSON(this.service.getByCode(code));
+  }
+
+  @Override
+  @Request(RequestType.SESSION)
+  public JsonObject create(String sessionId, JsonObject object)
+  {
+    BusinessEdgeType type = this.service.create(object);
+
+    return this.service.toJSON(type);
+  }
+}
