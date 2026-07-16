cab735b9f84813243d9fe924febea300c603e243
terraframe-builder <builder@terraframe.com>
2026-07-16 21:45:58 +0000

    License headers

--- geoprism-labeled-property-graph/src/main/java/net/geoprism/registry/view/AttributeFloatDTO.java
+++ geoprism-labeled-property-graph/src/main/java/net/geoprism/registry/view/AttributeFloatDTO.java
@@ -1,21 +1,20 @@
 /**
- * Copyright (c) 2022 TerraFrame, Inc. All rights reserved.
+ * Copyright (c) 2023 TerraFrame, Inc. All rights reserved.
  *
- * This file is part of Common Geo Registry Adapter(tm).
+ * This file is part of Geoprism(tm).
  *
- * Common Geo Registry Adapter(tm) is free software: you can redistribute it
- * and/or modify it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version.
+ * Geoprism(tm) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- * Common Geo Registry Adapter(tm) is distributed in the hope that it will be
- * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
- * General Public License for more details.
+ * Geoprism(tm) is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Common Geo Registry Adapter(tm). If not, see
- * <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Geoprism(tm).  If not, see <http://www.gnu.org/licenses/>.
  */
 package net.geoprism.registry.view;
 
