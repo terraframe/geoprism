303a7113b50405af013add6e576f6c45612c24ec
jsmethie <jsmethie@terraframe.com>
2026-09-17 14:18:10 -0600

    chore: made authority and data source code immutable

--- geoprism-labeled-property-graph/src/main/java/net/geoprism/registry/service/business/SourceAuthorityBusinessService.java
+++ geoprism-labeled-property-graph/src/main/java/net/geoprism/registry/service/business/SourceAuthorityBusinessService.java
@@ -3,18 +3,18 @@
  *
  * This file is part of Geoprism(tm).
  *
- * Geoprism(tm) is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * Geoprism(tm) is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
  *
- * Geoprism(tm) is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * Geoprism(tm) is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with Geoprism(tm).  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Geoprism(tm). If not, see <http://www.gnu.org/licenses/>.
  */
 package net.geoprism.registry.service.business;
 
@@ -99,9 +99,9 @@ public class SourceAuthorityBusinessService implements SourceAuthorityBusinessSe
     else
     {
       source = new SourceAuthority();
+      source.setCode(dto.getCode());
     }
 
-    source.setCode(dto.getCode());
     LocalizedValueConverter.populate(source, SourceAuthority.DISPLAYLABEL, dto.getLabel());
     LocalizedValueConverter.populate(source, SourceAuthority.DESCRIPTION, dto.getDescription());
 
